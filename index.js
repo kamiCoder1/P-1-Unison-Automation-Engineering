e9dc17824009f66422a65d54cb2f74327448ccd6
kamranpersonal <kami_22@rocketmail.com>
2022-05-15 00:53:52 +0500

    commiting deleted unnecessary files after creation of header footer and slider

---
+++ index.js
@@ -0,0 +1,45 @@
+const 
+    express = require('express'),
+    app = express(),
+    port = 8080;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname+"\\public"));
+
+app.use('/',(req, res, next)=>{
+    console.log("app.use() session check");
+    next();
+})
+
+app.get('/about', (req, res) => {
+    console.log("about us");
+    res.render('about', { root: __dirname });
+});
+
+app.get('/home', (req, res) => {
+    console.log("home");    
+    console.log(`root=${__dirname}`+"\\public");
+    res.render('home', { root: __dirname });
+});
+
+app.get('/contact', (req, res) => {
+    console.log("contact");
+    res.render('contact', { root: __dirname });
+});
+
+app.get('/services', (req, res) => {
+    console.log("services");    
+    console.log(`root=${__dirname}`+"\\public");
+    res.render('services', { root: __dirname });
+});
+
+app.get('/projects', (req, res) => {
+    console.log("projects");
+    res.render('projects', { root: __dirname });
+});
+
+app.listen(port, ()=>{
+    console.log(`Example app listening on port ${port}`);
+});
+
